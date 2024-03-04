2d63f96068a66d62b96d90918835ecadbff9eb83
speckitor <pasaloginov381@gmail.com>
2024-03-04 21:20:27 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,56 @@
+import flet as ft
+import tensorflow as tf
+from PIL import Image
+
+def main(page: ft.Page):
+    page.title = "Pneumonia Checker"
+    page.window_width = 700
+    page.window_height = 350
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    def pick_file_and_predict(e: ft.FilePickerResultEvent):
+        model = tf.keras.models.load_model("assets/model_files/")
+        prediction.value = ""
+        result_file = e.files
+        path_to_image = result_file[0].path
+
+        image.src = result_file[0].path
+        image.update()
+
+        selected_files.value = (f"file path: {path_to_image}")
+        selected_files.update()
+
+        image_tf = tf.keras.preprocessing.image.load_img(path_to_image)
+        image_tf = tf.image.resize(image_tf, size=(512, 512))
+        image_tf = tf.reshape(image_tf, (512, 512, 3))
+        image_tf = tf.expand_dims(image_tf, axis=0)
+        image_prediction = model.predict(image_tf)[0]
+
+        if image_prediction[1]>image_prediction[0] and image_prediction[1]<0.7:
+            prediction.value = f"RESULT: probably pneumonia ({round(float(image_prediction[1]), 1) * 100}% pneumonia chance)"
+        elif image_prediction[1]>image_prediction[0] and image_prediction[1]>0.7:
+            prediction.value = f"RESULT: pneumonia ({round(float(image_prediction[1]), 1) * 100}% pneumonia chance)"
+        else:
+            prediction.value = f"RESULT: clear ({round(float(image_prediction[1]), 1) * 100}% pneumonia chance)"
+        prediction.update()
+
+    
+    pick_files_dialog = ft.FilePicker(on_result=pick_file_and_predict)
+    selected_files = ft.Text("no_image_imported")
+    image = ft.Image(src="assets/no_image.png",width=100,height=100,fit=ft.ImageFit.CONTAIN)
+    prediction = ft.Text()
+    
+    page.overlay.append(pick_files_dialog)
+
+    page.add(
+        ft.Row([
+                ft.ElevatedButton("import image", icon=ft.icons.UPLOAD_FILE,
+                on_click=lambda _: pick_files_dialog.pick_files()),
+                ], alignment=ft.MainAxisAlignment.CENTER),
+        ft.Divider(),
+        ft.Row([image, selected_files], alignment=ft.MainAxisAlignment.CENTER),
+        ft.Divider(),
+        ft.Row([prediction], alignment=ft.MainAxisAlignment.CENTER),
+    )
+    
+
+ft.app(target=main)
---
+++ main_model.py
@@ -0,0 +1,56 @@
+import tensorflow as tf
+import os
+os.environ["TF_CPP_MIN_LOG_LEVEL"]="2"
+#используйте 2 строчки ниже, если у вас видеокарта от nvidia
+physical_devices = tf.config.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
+batch_size = 16
+img_size = (512, 512)
+
+# Напишите свой путь до датасета в переменных ниже
+val_dir = "/home/spectr/Downloads/chest_xray/test" 
+train_dir = "/home/spectr/Downloads/chest_xray/train"
+test_dir="/home/spectr/Downloads/chest_xray/val"
+
+train_data = tf.keras.utils.image_dataset_from_directory(
+    train_dir,
+    seed = 42,
+    batch_size=batch_size,
+    image_size =img_size)
+validation_data = tf.keras.utils.image_dataset_from_directory(
+    val_dir,
+    seed = 42,
+    batch_size=batch_size,
+    image_size =img_size)
+test_data = tf.keras.utils.image_dataset_from_directory(
+    test_dir,
+    seed = 42,
+    batch_size=batch_size,
+    image_size=img_size)
+
+class_names_num = len(train_data.class_names)
+
+tf.random.set_seed(42)
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Rescaling(scale=1./255),
+    
+    tf.keras.layers.Conv2D(16, (5,5), activation="relu", padding="same"),
+    tf.keras.layers.MaxPooling2D((2, 2)),
+
+    tf.keras.layers.Conv2D(32, (5,5), activation="relu", padding="same"),
+    tf.keras.layers.MaxPooling2D((2, 2)),
+
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(64, activation="relu"),
+    tf.keras.layers.Dense(128, activation="relu"),
+    tf.keras.layers.Dense(class_names_num, activation="softmax")
+])
+model.compile(optimizer=tf.keras.optimizers.Adam(),
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+model.fit(train_data, validation_data=validation_data, epochs=3)
+
+model.evaluate(test_data)
+
+#model.save("assets/model_files")
